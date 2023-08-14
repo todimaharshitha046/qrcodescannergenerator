939b5547cf712d8f4ce7e8c9814c1a115649b106
todimaharshitha046 <107941419+todimaharshitha046@users.noreply.github.com>
2023-08-14 23:20:43 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pyqrcode
 import cv2
-import webbrowser
 
 def scan_qr():
+    st.write("Scanning QR Code...")
     cap = cv2.VideoCapture(0)
     detector = cv2.QRCodeDetector()
     while True:
@@ -12,6 +12,7 @@ def scan_qr():
         if data:
             cap.release()
             cv2.destroyAllWindows()
+            st.write("QR Code Scanned:", data)
             return data
         st.image(img, channels="BGR", use_column_width=True)
         if cv2.waitKey(1) == 113:  # ASCII value of 'q'
@@ -21,7 +22,7 @@ def generate_qr(text):
     if len(text) != 0:
         qr = pyqrcode.create(text)
         st.image(qr.xbm(scale=8), channels="L", use_column_width=True)
-        st.write("QR code of " + text)
+        st.write("Generated QR Code for:", text)
     else:
         st.warning('All Fields are Required!')
 
@@ -34,9 +35,7 @@ def main():
         user_input = st.text_input("Enter message or URL")
         generate_qr(user_input)
     elif action == "Scan QR Code":
-        data = scan_qr()
-        if data:
-            webbrowser.open(data)
+        scan_qr()
 
 if __name__ == "__main__":
     main()
